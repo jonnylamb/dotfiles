f13148f93f2c016c0268e38b34396c6abf5aa79b
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-10-03 23:19:39 +0100

    Changed qwerty-on-dvorak tag switching keybindings to be more normal.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -213,8 +213,8 @@ if dvorak then
     keybinding({ modkey }, "less", awful.tag.viewnext):add()
 
     -- These bindings are for qwerty keyboards on a dvorak layout (e.g. when using synergy)
-    keybinding({ "Mod4" }, "Left", awful.tag.viewprev):add()
-    keybinding({ "Mod4" }, "Right", awful.tag.viewnext):add()
+    keybinding({ "Mod4" }, "q", awful.tag.viewprev):add()
+    keybinding({ "Mod4" }, "w", awful.tag.viewnext):add()
 
 else
     keybinding({ modkey }, "q", awful.tag.viewprev):add()
