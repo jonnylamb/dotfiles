b53f8893e6a0c64d16b9c7022366f61bef02d390
Jonny Lamb <jonny@debian.org>
2009-06-15 21:29:11 +0100

    awesome/theme.lua: Use darklooks colours.
    Signed-off-by: Jonny Lamb <jonny@debian.org>

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -6,19 +6,19 @@ theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#535551"
+theme.bg_focus      = "#2c3436"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#ffffff"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize  = " #ffffff"
 
 theme.border_width  = "1"
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_focus  = "#3d3e3c"
 theme.border_marked = "#91231c"
 
 -- There are another variables sets
