de0e9bd05c19ab9876e4a76a8c37d9e41ad61301
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-21 23:54:27 +0100

    emacs.d/config/utf8.el: simplify

--- emacs.d/config/utf8.el
+++ emacs.d/config/utf8.el
@@ -1,13 +1,6 @@
 ;; -*- emacs-lisp -*-
 
-; utf8
-(if (string-match "[Uu][Tt][Ff]-?8" (or (getenv "LC_ALL")
-                                        (getenv "LANG")
-                                        (format "")))
-    (progn
-      (set-language-environment "UTF-8")
-      (if (not window-system)
-          (progn
-            (set-keyboard-coding-system 'utf-8)
-            (set-terminal-coding-system 'utf-8))))
-  (set-language-environment "Latin-1"))
+;; utf8
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
