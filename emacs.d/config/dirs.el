30c2c8f292e3f0f66351b3276f023f5f38c86bb3
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-21 20:26:34 +0100

    emacs.d/custom.el: store custom-set-variables in its own file

--- emacs.d/config/dirs.el
+++ emacs.d/config/dirs.el
@@ -8,3 +8,7 @@
 ; set themes dir
 (setq jonny/themes-base-dir
       (expand-file-name (file-name-as-directory "themes") user-emacs-directory))
+
+; keep custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
---
+++ emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0eebf69ceadbbcdd747713f2f3f839fe0d4a45bd0d4d9f46145e40878fc9b098" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
