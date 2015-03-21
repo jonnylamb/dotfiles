3766f4e03746ca40b67ce76c0266611e1395310c
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-21 19:20:24 +0100

    emacs.d/themes/monokai: add as a submodule

--- emacs.d/config/dirs.el
+++ emacs.d/config/dirs.el
@@ -4,3 +4,7 @@
 (setq site-lisp-dir
       (expand-file-name "site-lisp" user-emacs-directory))
 (add-to-list 'load-path site-lisp-dir)
+
+; set themes dir
+(add-to-list 'custom-theme-load-path
+             (expand-file-name "themes" user-emacs-directory))
