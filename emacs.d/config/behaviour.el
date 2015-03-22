f483ef96f03a9e2838ad6bc61be3be7f8ba8e0fb
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-22 18:09:31 +0100

    emacs.d/config/behaviour.el: offer to create parent directories

--- emacs.d/config/behaviour.el
+++ emacs.d/config/behaviour.el
@@ -11,3 +11,12 @@
 (require 'saveplace)
 (setq save-place-file (expand-file-name "saved-places" user-emacs-directory))
 (setq-default save-place t)
+
+;; offer to create parent directories if they do not exist
+;; http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
+(defun my-create-non-existent-directory ()
+  (let ((parent-directory (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p parent-directory))
+               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory t))))
+(add-to-list 'find-file-not-found-functions 'my-create-non-existent-directory)
