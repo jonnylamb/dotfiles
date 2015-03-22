5050e01d82ed1f17f6a93f1f51d22fcb269a20ff
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-03-22 18:07:21 +0100

    emacs.d/config/utf8.el: enable more utf8

--- emacs.d/config/utf8.el
+++ emacs.d/config/utf8.el
@@ -1,6 +1,8 @@
 ;; -*- emacs-lisp -*-
 
 ;; utf8
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
