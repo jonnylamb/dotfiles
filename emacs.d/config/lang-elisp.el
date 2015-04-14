bc48a57310249f1aa8ba36e1f06819c90b756c60
Jonny Lamb <jonnylamb@jonnylamb.com>
2015-04-14 16:51:04 +0000

    emacs.d/config/lang-elisp.el: enable electric-pair-mode in more lisp modes

--- emacs.d/config/lang-elisp.el
+++ emacs.d/config/lang-elisp.el
@@ -2,3 +2,5 @@
 
 ;; close brackets automatically
 (add-hook 'emacs-lisp-common-hook 'electric-pair-mode)
+(add-hook 'lisp-interaction-common-hook 'electric-pair-mode)
+(add-hook 'lisp-common-hook 'electric-pair-mode)
