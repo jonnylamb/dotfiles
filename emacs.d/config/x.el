0b25204570b4a526e90915c5061aef8b2eb683ba
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-02 22:22:39 +0100

    emacs.d/config/x.el: set a different paste function on mac

--- emacs.d/config/x.el
+++ emacs.d/config/x.el
@@ -2,4 +2,7 @@
 
 ;; use the X clipboard
 (setq x-select-enable-clipboard t)
+
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+(when 'jonny/is-mac
+  (setq interprogram-paste-function 'x-selection-value))
