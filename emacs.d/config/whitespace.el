675cea269d7406f1a4fcac69b0f0048900e2037c
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-02 21:39:46 +0100

    emacs.d/config/whitespace.el: add scala and thrift mode

--- emacs.d/config/whitespace.el
+++ emacs.d/config/whitespace.el
@@ -3,5 +3,5 @@
 ;; show tabs and stuff
 (require 'whitespace)
 (global-whitespace-mode t)
-(setq whitespace-global-modes '(c-mode c++-mode sh-mode emacs-lisp-mode makefile-mode python-mode))
+(setq whitespace-global-modes '(c-mode c++-mode sh-mode emacs-lisp-mode makefile-mode python-mode scala-mode thrift-mode))
 (setq whitespace-style '(face tabs spaces trailing space-before-tab empty space-after-tab::tab space-mark tab-mark))
