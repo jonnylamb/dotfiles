9e6d279430d348ee63b6cbc1457e13e2b4515449
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-03 18:17:50 +0100

    emacs.d/config/whitespace.el: reformat

--- emacs.d/config/whitespace.el
+++ emacs.d/config/whitespace.el
@@ -2,6 +2,28 @@
 
 ;; show tabs and stuff
 (require 'whitespace)
+
+;; maybe this should be less of a whitelist and perhaps a blacklist...
+(setq whitespace-global-modes
+      '(c-mode
+        c++-mode
+        sh-mode
+        emacs-lisp-mode
+        makefile-mode
+        python-mode
+        scala-mode
+        thrift-mode))
+
+;; don't enable everything (like end of line markers)
+(setq whitespace-style
+      '(face
+        tabs
+        spaces
+        trailing
+        space-before-tab
+        empty
+        space-after-tab::tab
+        space-mark
+        tab-mark))
+
 (global-whitespace-mode t)
-(setq whitespace-global-modes '(c-mode c++-mode sh-mode emacs-lisp-mode makefile-mode python-mode scala-mode thrift-mode))
-(setq whitespace-style '(face tabs spaces trailing space-before-tab empty space-after-tab::tab space-mark tab-mark))
