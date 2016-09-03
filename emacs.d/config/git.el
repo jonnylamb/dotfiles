bd5507c3297e44888479054d7d3f7549d23e35bc
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-03 17:27:28 +0100

    emacs.d/config/git.el: remove some unused code

--- emacs.d/config/git.el
+++ emacs.d/config/git.el
@@ -2,9 +2,3 @@
 
 ;; open commit messages in diff-mode
 (setq auto-mode-alist (cons '("COMMIT_EDITMSG" . diff-mode) auto-mode-alist))
-
-;; git diff buffer
-;; this doesn't work and I don't know why
-;;(jonny/when-installed
-;; 'git-gutter
-;; (global-git-gutter-mode t))
