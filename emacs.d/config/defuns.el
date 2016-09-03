c2c1c82bf941e04760dcb74939f2b9f2cfede492
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-03 17:29:35 +0100

    emacs.d/config/defuns.el: remove soon-to-be unused code
    This breaks things but the submodules have already gone so it's
    already broken.

--- emacs.d/config/defuns.el
+++ emacs.d/config/defuns.el
@@ -1,30 +1,5 @@
 ;; -*- emacs-lisp -*-
 
-(require 's)
-
-;; misc useful functions
-(defun jonny/first-char (s)
-  "first char of string"
-  (s-left 1 s))
-
-;; work out whether submodules are loaded
-(defun jonny/test-submodules/list ()
-  "list of submodules"
-  (s-lines (s-trim-right (shell-command-to-string "git submodule status"))))
-
-(defun jonny/test-submodules/statuses ()
-  "list of submodule statuses"
-  (mapcar 'jonny/first-char (jonny/test-submodules/list)))
-
-(defun jonny/test-submodules ()
-  "whether all submodules are in clean state in current working directory"
-  (s-blank? (s-trim (s-join "" (jonny/test-submodules/statuses)))))
-
-(defun jonny/when-installed (name &rest body)
-  "test whether a package is installed and if so, run body"
-  (when (require name nil 'noerror)
-    body))
-
 (defun jonny/is-mac (&rest body)
   (if (string-equal system-name "darwin")
       body))
