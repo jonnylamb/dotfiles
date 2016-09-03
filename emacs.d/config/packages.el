2100ba06f012c210819f7aa81ca9fc09d5c7c717
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-03 17:32:27 +0100

    emacs.d/config/packages.el: set up package.el with melpa and use-package

--- emacs.d/config/packages.el
+++ emacs.d/config/packages.el
@@ -1,9 +1,22 @@
 ;; -*- emacs-lisp -*-
 
+(require 'package)
+
 ;; remove gnu
 (setq package-archives nil)
 
-;; add marmalade
-(add-to-list
- 'package-archives
- '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; add melpa
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; go
+(package-initialize)
+
+;; only do this if the archive isn't already there as updates are boring.
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; now use use-package after this
+(require 'use-package)
+(setq use-package-always-ensure t)
