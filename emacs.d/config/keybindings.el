54b9222d2d06d8b16a71e3282aa88ca238327610
Jonny Lamb <jonnylamb@jonnylamb.com>
2016-09-14 15:33:03 +0100

    emacs.d/config/keybindings.el: use a more sensible mc/mark-all-like-this binding

--- emacs.d/config/keybindings.el
+++ emacs.d/config/keybindings.el
@@ -18,7 +18,7 @@
 (use-package multiple-cursors
   :bind (("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+         ("C-c C->" . mc/mark-all-like-this)))
 
 ;; smex
 (use-package smex
