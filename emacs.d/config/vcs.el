21e913ddeedbd3accf0455c6189c35d00dcfcffb
Jonny Lamb <jonnylamb@jonnylamb.com>
2017-06-24 15:47:22 +0100

    emacs.d/config/vcs.el: disable git

--- emacs.d/config/vcs.el
+++ emacs.d/config/vcs.el
@@ -2,3 +2,7 @@
 
 ;; follow symlinks
 (setq vc-follow-symlinks t)
+
+;; disable Git because it takes the lockfile which can get in the way
+;; with huge slow repos
+(delete 'Git vc-handled-backends)
